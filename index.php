41b68c7bd115b148fc656d2b2b6a3be4289180fc
Tom <domagala.tomasz@gmail.com>
2021-02-11 13:13:25 +0100

    tasks

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require "vendor/autoload.php";
+require_once (__DIR__."/vendor/autoload.php");
 
 echo("<h1>How to use Environmental Variables (phpdotenv for php1)</h1>");
 
@@ -9,7 +9,11 @@ $dotenv->load();
 
 /*$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);*/
 /*wors only with: createUnsafeImmutable(__DIR__);*/
+
 echo("<li>getenv():".getenv());
+
+$secret_key = getenv("SECRET_KEY");
+echo ("<li> new: ". $secret_key);
 
 // echo("<li> ENV key: ".$_ENV['SECRET_KEY']);
 // echo("<li> ENV pass: ".$_ENV['PASSWORD']);
