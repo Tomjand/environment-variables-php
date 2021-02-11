181dd8e24a5a1127abecd952c4f70414195c4992
Tom <domagala.tomasz@gmail.com>
2021-02-11 16:14:48 +0100

    a

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
 // require_once (__DIR__."/vendor/autoload.php");
-require_once ("vendor/autoload.php");
+//require_once ("vendor/autoload.php");
 
 echo("<h1>How to use Environmental Variables (phpdotenv for php2)</h1>");
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+//$dotenv->load();
 
 /*$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);*/
 /*wors only with: createUnsafeImmutable(__DIR__);*/
@@ -16,7 +16,7 @@ echo("<li>getenv():".getenv());
 $secret_key = getenv("SECRET_KEY");
 echo ("<li> new: ". $secret_key);
 
-// echo("<li> ENV key: ".$_ENV['SECRET_KEY']);
+echo("<li> ENV key: ".$_ENV['SECRET_KEY']);
 // echo("<li> ENV pass: ".$_ENV['PASSWORD']);
 // echo("<li> SERVER: ".$_SERVER['PASSWORD']);
 ?>
