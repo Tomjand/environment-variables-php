7e945b9fbcd2dbd52fc34db0e53f520646f3fb21
Tom <domagala.tomasz@gmail.com>
2021-02-23 11:06:51 +0100

    http_host

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//require_once ("config.php");
+require_once ("config.php");
  //require_once (__DIR__."/vendor/autoload.php");
 //require_once ("vendor/autoload.php");
 
@@ -16,6 +16,6 @@ echo("<h1>How to use Environmental Variables (phpdotenv for php2)</h1>");
 
 // echo("<li> ENV key: ".$_ENV['SECRET_KEY']);
 // echo("<li> ENV pass: ".$_ENV['PASSWORD']);
-// echo("<li> SERVER: ".$_SERVER['HTTP_HOST']);
+echo("<li> SERVER: ".$_SERVER['HTTP_HOST']);
 echo("<li> SERVER passsword: ".$_SERVER['pass']);
 ?>
